280440783d6ccf7a6aa2307f8556f009a10c773d
Dima Guba <gudmian@gmail.com>
2017-06-21 14:12:58 +0300

    Created frame "MainFrame"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import frames.MainFrame;
+
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        JFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+
     }
 }
---
+++ src/frames/MainFrame.java
@@ -0,0 +1,13 @@
+package frames;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame(){
+        super();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600, 400);
+    }
+
+}
